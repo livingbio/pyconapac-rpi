66166585aedd5530ab04fd5af1b87cba398beaf6
lucemia <lucemia@gmail.com>
2015-08-16 17:39:07 +0800

    update all config

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -1,46 +1,50 @@
-from fabric.api import run, env,local, sudo,cd, parallel
+from fabric.api import run, env,local, sudo,cd, parallel, put
 from fabric.contrib import files
 from fabric.operations import reboot
 import re
 
 ## get all pi ips
-def get_ips():
-    result = local('nmap -sV -p 22 192.168.2.0/24', capture=True)
+def get_ips(subnet):
+    result = local('nmap -sV -p 22 %s/24' % subnet, capture=True)
     ips = result.replace("\n", "@").split('Nmap scan report for ')
     ips = [ip.split('@')[0] for ip in ips if 'Debian 4+deb7u2' in ip]
-    print ips
-    try:
-        ips.remove('192.168.2.80')
-    except:
-        pass
+
     return ips
 
-
+env.hosts = get_ips(env.subnet)
+print 'scaned hosts', env.hosts
 env.user = "pi"
 env.password = "raspberry"
-env.hosts = list(set(get_ips() + get_ips()))
-
 
 def update_cron():
+
     # override the crontab
     sudo('rm -rf /var/spool/cron/crontabs/*')
-    sudo('echo "%s" > /var/spool/cron/crontabs/root' % open('./crontab').read())
-    sudo('chmod 600 /var/spool/cron/crontabs/root')
+    put('./crontab', '/var/spool/cron/crontabs/root', use_sudo=True, mode=0600)
+    # sudo('echo "%s" > /var/spool/cron/crontabs/root' % open('./crontab').read())
+    # sudo('chmod 600 /var/spool/cron/crontabs/root')
 
     print '====================================================================='
     sudo("cat /var/spool/cron/crontabs/root")
     print '====================================================================='
 
 def install_requirements():
-    with open('./install') as ifile:
+    with open('./install.sh') as ifile:
         for iline in ifile:
             sudo(iline)
+
+def update_network():
+    put('./interfaces', '/etc/network/interfaces', use_sudo=True, mode=644)
+    # sudo('echo "%s" > /etc/network/interfaces' % open('./interfaces').read())
+    # sudo('chmod 644 /etc/network/interfaces')
+    put('./wpa_supplicant.conf', '/etc/wpa_supplicant/wpa_supplicant.conf', use_sudo=True, mode=600)
+
+    # sudo(r'echo "%s" > /etc/wpa_supplicant/wpa_supplicant.conf' % open('./wpa_supplicant.conf').read())
+    # sudo('chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf')
 
 @parallel
 def setup():
     install_requirements()
     update_cron()
+    update_network()
     reboot()
-    print 'end'
-
-
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,8 @@
 apt-get update
 apt-get install -y vim python-dev python-pip
 pip install spidev requests
-mkdir .ssh || true
-echo -e 'Host github.com\\n\\tStrictHostKeyChecking no\\n' > ~/.ssh/config
+mkdir /home/pi/.ssh || true
+echo -e 'Host github.com\\n\\tStrictHostKeyChecking no\\n' > /home/pi/.ssh/config
 rm -rf /home/pi/SPI-Py || true
-git clone https://github.com/lthiery/SPI-Py /home/pi/SPI-Py 
-cd /home/pi/SPI-Py
-python setup.py install
+git clone https://github.com/lthiery/SPI-Py /home/pi/SPI-Py
+cd /home/pi/SPI-Py && python setup.py install

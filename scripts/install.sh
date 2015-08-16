1f1e1aacb0e53fde51014c0e8b800b85ce181c6a
lucemia <lucemia@gmail.com>
2015-08-16 07:38:55 +0000

    fix the install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,7 @@ apt-get install -y vim python-dev python-pip
 pip install spidev requests
 mkdir .ssh || true
 echo -e 'Host github.com\\n\\tStrictHostKeyChecking no\\n' > ~/.ssh/config
-rm -rf SPI-Py || true
-git clone https://github.com/lthiery/SPI-Py
-python ./SPI-Py/setup.py install
+rm -rf /home/pi/SPI-Py || true
+git clone https://github.com/lthiery/SPI-Py /home/pi/SPI-Py 
+cd /home/pi/SPI-Py
+python setup.py install
